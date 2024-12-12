d2ad7c04f5f1f8a5f0d59de1363ad97da703b49d
RKolar <bato9440@hotmail.com>
2024-12-12 22:01:55 +0100

    Cleanup App and adding validation

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import './App.css';
 import SearchMovie from "./components/SearchMovie/SearchMovie";
 
-// http://www.omdbapi.com/?apikey=[yourkey]&
-console.log(process.env.REACT_APP_OMDBAPI_KEY, process.env.REACT_APP_OMDBAPI_URL)
-
-// axios.get(`${process.env.REACT_APP_OMDBAPI_URL}?t=Terminator&apikey=${process.env.REACT_APP_OMDBAPI_KEY}`)
-//     .then(response => {
-//         console.log(response.data);
-//     }).catch(error => {
-//         console.log(error);
-// })
-
 function App() {
   return (
     <div className="App">
--- src/components/SearchMovie/SearchMovie.js
+++ src/components/SearchMovie/SearchMovie.js
@@ -3,21 +3,40 @@ import {fetchMovie} from "../../services/omdbApi";
 
 const SearchMovie = () => {
 
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
     const [title, setTitle] = useState("");
 
     const handleSearchMovie = async (e) => {
         e.preventDefault();
+        setError(null);
+
+        if(title.trim() === "") {
+            setError("Please enter a movie title.");
+            return;
+        }
+        if(title.length < 3) {
+            setError("The movie title must be at least 3 characters.");
+            return;
+        }
+
+        setIsLoading(true);
         try {
            await fetchMovie(title);
+            setTitle("");
+
         } catch (e) {
             console.log(e);
+        } finally {
+            setIsLoading(false);
         }
     }
 
     return (
         <>
-        <input id="movieSearch" type="text" onChange={e => setTitle(e.target.value)} placeholder="Search title movie..." />
-            <button onClick={handleSearchMovie}>Search movie</button>
+        <input value={title} type="text" onChange={e => setTitle(e.target.value)} placeholder="Search title movie..." />
+            <button onClick={handleSearchMovie} disabled={isLoading}>{isLoading ? "Loading..." : "Search movie"}</button>
+            {error && <p style={{ color: "red"}}>{error}</p>}
         </>
     )
 }
